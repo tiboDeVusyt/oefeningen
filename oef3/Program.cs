1d67d68ada5c14be4fd39e78b75f5e2a34006d41
Intec <Intec@PC215-10>
2021-10-05 14:54:36 +0200

    Add project files.

---
+++ oef3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oef3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int nr;
+            int nr1;
+            int nr2;
+            Console.WriteLine("write a number");
+            nr = int.Parse(Console.ReadLine());
+
+
+            nr1 = nr * nr;
+            nr2 = nr * nr * nr;
+
+            Console.WriteLine(nr1);
+            Console.WriteLine(nr2);
+            Console.ReadLine();
+        }
+    }
+}
